1eb9f439fbb77528fb48f40fa1be0f722647809d
Amber Perrine <165063290+avperrine@users.noreply.github.com>
2024-10-08 14:21:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,5 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_ord
 # New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
